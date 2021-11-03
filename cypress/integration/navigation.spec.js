b89657d7ff8fc9ed1440ede3e1ef065cca1236e7
Paul Shanahan <20083717@mail.wit.ie>
2021-11-03 12:18:27 +0000

    E2E tests - Navigation - Backward/Forward between the home page and movie details page.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -34,5 +34,54 @@ describe("Navigation", () => {
       cy.url().should("include", `/movies/${movies[0].id}`);
       cy.get("h3").contains(movies[0].title);
     });
-  });
-});
+    describe("The site header", () => {
+        describe("when the viewport is desktop scale", () => {
+          it("should allow navigation to the Favourites page from the link", () => {
+            cy.get("header").find(".MuiToolbar-root").find("button").eq(1).click();
+            cy.url().should("include", `/favorites`);
+            cy.get("h3").contains("Favourite Movies");
+          });
+        });
+        describe(
+          "when the viewport is a mobile",
+          {
+            viewportHeight: 896,
+            viewportWidth: 414,
+          },
+          () => {
+            it("should allow navigation to the Favourites page from the dropdown menu", () => {
+              cy.get("header").find("button").click();
+              cy.get("li").eq(1).click();
+              cy.url().should("include", `/favorites`);
+              cy.get("h3").contains("Favourite Movies");
+            });
+          });
+          describe("From the Favorites page", () => {
+            beforeEach(() => {
+              cy.get("button[aria-label='add to favorites']").eq(0).click();
+              cy.get("button[aria-label='add to favorites']").eq(1).click();
+              cy.get("header").find(".MuiToolbar-root").find("button").eq(1).click();
+            });
+            it("should navigate to the movies detail page and change the browser URL", () => {
+              cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+              cy.url().should("include", `/movies/${movies[0].id}`);
+              cy.get("h3").contains(movies[0].title);
+            });
+            describe("The forward/backward links", () => {
+                beforeEach(() => {
+                  cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+                });
+                it("should navigate backward and forward between the movies detail page and the Discover page.", () => {
+                  cy.get("button[aria-label='go back'").click();
+                  cy.get("h3").contains("Discover Movies");
+                  cy.url().should("not.include", `/movies/${movies[0].id}`);
+                  cy.get("button[aria-label='go forward'").click();
+                  cy.url().should("include", `/movies/${movies[0].id}`);
+                  cy.get("h3").contains(movies[0].title);
+                });
+              });
+            });
+          });
+        });
+      });
+    
